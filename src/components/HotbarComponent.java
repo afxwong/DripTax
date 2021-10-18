ab43e672c8db29ea1b282fd3af7310d61343b994
Anthony Wong <awong307@gatech.edu>
2021-10-17 21:52:16 -0400

    checkstyle fixes

--- src/components/HotbarComponent.java
+++ src/components/HotbarComponent.java
@@ -19,44 +19,56 @@ public class HotbarComponent extends HBox {
         this.setLayoutX(0);
         this.setLayoutY(400);
 
-        ImageView FireTowerLabel = new ImageView(new Image("resources/fireTower.png", 100, 100, false, false));
-        ImageView WaterTowerLabel = new ImageView(new Image("resources/waterTower.png", 100, 100, false, false));
-        ImageView GroundTowerLabel = new ImageView(new Image("resources/groundTower.png", 100, 100, false, false));
-        ImageView AirTowerLabel = new ImageView(new Image("resources/airTower.png", 100, 100, false, false));
+        ImageView fireTowerLabel = new ImageView(new Image(
+                "resources/fireTower.png", 100, 100, false, false));
+        ImageView waterTowerLabel = new ImageView(new Image(
+                "resources/waterTower.png", 100, 100, false, false));
+        ImageView groundTowerLabel = new ImageView(new Image(
+                "resources/groundTower.png", 100, 100, false, false));
+        ImageView airTowerLabel = new ImageView(new Image(
+                "resources/airTower.png", 100, 100, false, false));
 
-        Tooltip.install(FireTowerLabel, new Tooltip("Fire Tower - deals damage over time by applying Burn status effect"));
-        Tooltip.install(WaterTowerLabel, new Tooltip("Water Tower - slows enemies, ice upgrade that freezes enemies"));
-        Tooltip.install(GroundTowerLabel, new Tooltip("Ground Tower - splash damage"));
-        Tooltip.install(AirTowerLabel, new Tooltip("Air Tower - fast attack, low damage"));
-        FireTowerLabel.setOnMouseClicked(mouseEvent -> placeTower(Element.Fire));
-        WaterTowerLabel.setOnMouseClicked(mouseEvent -> placeTower(Element.Water));
-        GroundTowerLabel.setOnMouseClicked(mouseEvent -> placeTower(Element.Ground));
-        AirTowerLabel.setOnMouseClicked(mouseEvent -> placeTower(Element.Air));
-        FireTowerLabel.setPickOnBounds(true);
-        WaterTowerLabel.setPickOnBounds(true);
-        GroundTowerLabel.setPickOnBounds(true);
-        AirTowerLabel.setPickOnBounds(true);
-        this.getChildren().addAll(FireTowerLabel, WaterTowerLabel, GroundTowerLabel, AirTowerLabel);
+        Tooltip.install(fireTowerLabel, new Tooltip(
+                "Fire Tower\nDeals damage over time by applying Burn status effect"));
+        Tooltip.install(waterTowerLabel, new Tooltip(
+                "Water Tower\nSlows enemies, ice upgrade that freezes enemies"));
+        Tooltip.install(groundTowerLabel, new Tooltip(
+                "Ground Tower\nSplash damage of ground type"));
+        Tooltip.install(airTowerLabel, new Tooltip(
+                "Air Tower\nFast attack, low damage attack of air type"));
+        fireTowerLabel.setOnMouseClicked(mouseEvent -> placeTower(Element.Fire));
+        waterTowerLabel.setOnMouseClicked(mouseEvent -> placeTower(Element.Water));
+        groundTowerLabel.setOnMouseClicked(mouseEvent -> placeTower(Element.Ground));
+        airTowerLabel.setOnMouseClicked(mouseEvent -> placeTower(Element.Air));
+        fireTowerLabel.setPickOnBounds(true);
+        waterTowerLabel.setPickOnBounds(true);
+        groundTowerLabel.setPickOnBounds(true);
+        airTowerLabel.setPickOnBounds(true);
+        this.getChildren().addAll(fireTowerLabel, waterTowerLabel, groundTowerLabel, airTowerLabel);
         this.setAlignment(Pos.CENTER);
     }
 
     private void placeTower(Element towerType) {
         boolean sufficientRunes = false;
-        if (towerType == Element.Fire && hasSufficientRuins(GameConfig.getFrune(), GameConfig.getTowerCost())) {
+        if (towerType == Element.Fire && hasSufficientRuins(
+                GameConfig.getFrune(), GameConfig.getTowerCost())) {
             GameConfig.setFrune(GameConfig.getFrune() - GameConfig.getTowerCost());
             sufficientRunes = true;
-        } else if (towerType == Element.Water && hasSufficientRuins(GameConfig.getWrune(), GameConfig.getTowerCost())) {
+        } else if (towerType == Element.Water && hasSufficientRuins(
+                GameConfig.getWrune(), GameConfig.getTowerCost())) {
             GameConfig.setWrune(GameConfig.getWrune() - GameConfig.getTowerCost());
             sufficientRunes = true;
-        } else if (towerType == Element.Ground && hasSufficientRuins(GameConfig.getGrune(), GameConfig.getTowerCost())) {
+        } else if (towerType == Element.Ground && hasSufficientRuins(
+                GameConfig.getGrune(), GameConfig.getTowerCost())) {
             GameConfig.setGrune(GameConfig.getGrune() - GameConfig.getTowerCost());
             sufficientRunes = true;
-        } else if (towerType == Element.Air && hasSufficientRuins(GameConfig.getArune(), GameConfig.getTowerCost())) {
+        } else if (towerType == Element.Air && hasSufficientRuins(
+                GameConfig.getArune(), GameConfig.getTowerCost())) {
             GameConfig.setArune(GameConfig.getArune() - GameConfig.getTowerCost());
             sufficientRunes = true;
         }
         if (sufficientRunes) {
-            GameScreen.infoPanel.updateInfo();
+            GameScreen.getInfoPanel().updateInfo();
             int[] selectedTile = Player.getSelectedTile();
             TowerGrid.placeTower(selectedTile, towerType);
             Player.deselectSelectedTile();
